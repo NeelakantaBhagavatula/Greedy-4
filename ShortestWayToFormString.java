3b5b200d2705c4eafabb0ce5c6e8f7b39cc16ea4
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-10-23 21:28:13 -0700

    Shortest Way to Form String (Greedy)

---
+++ ShortestWayToFormString.java
@@ -0,0 +1,93 @@
+// Time Complexity : O(nlogk) where n = length of target string, k = average number of character in source string
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+//1055. Shortest Way to Form String (Medium) - https://leetcode.com/problems/shortest-way-to-form-string/
+// Time Complexity : O(m*n) where m = length of source string, n = length of target string
+// Space Complexity : O(1)
+//class Solution {
+//	public int shortestWay(String source, String target) {
+//		int sl = source.length(), tl = target.length();
+//		int count = 1;
+//		int sp = 0, tp = 0;
+//
+//		HashSet<Character> set = new HashSet<>(); // O(1)
+//
+//		for (char ch : source.toCharArray()) { // O(m)
+//			set.add(ch);
+//		}
+//
+//		while (tp < tl) { // O(n)
+//			char sourceChar = source.charAt(sp);
+//			char targetChar = target.charAt(tp);
+//
+//			if (!set.contains(targetChar))
+//				return -1;
+//
+//			if (sourceChar == targetChar) {
+//				sp++;
+//				tp++;
+//
+//				if (tp == tl)
+//					return count;
+//			} else {
+//				sp++;
+//			}
+//
+//			if (sp == sl) {
+//				sp = 0;
+//				count++;
+//			}
+//		}
+//
+//		return count;
+//	}
+//}
+
+// Time Complexity : O(nlogk) where n = length of target string, k = average number of character in source string
+// Space Complexity : O(1)
+class Solution {
+	public int shortestWay(String source, String target) {
+		int sl = source.length(), tl = target.length();
+		int count = 1;
+		int sp = 0, tp = 0;
+
+		HashMap<Character, List<Integer>> map = new HashMap<>(); // O(1)
+
+		for (int i = 0; i < sl; i++) { // O(m)
+			char ch = source.charAt(i);
+
+			if (!map.containsKey(ch)) {
+				map.put(ch, new ArrayList<>());
+			}
+			map.get(ch).add(i);
+		}
+
+		while (tp < tl) { // O(n)
+			char ch = target.charAt(tp);
+
+			if (!map.containsKey(ch))
+				return -1;
+
+			List<Integer> list = map.get(ch);
+			int k = Collections.binarySearch(list, sp); // k elements, O(logk)
+
+			if (k < 0) { // if element not found
+				k = -k - 1;
+			}
+
+			if (k >= list.size()) {
+				count++;
+				sp = list.get(0);
+			} else {
+				sp = list.get(k);
+			}
+			sp++;
+			tp++;
+		}
+
+		return count;
+	}
+}
